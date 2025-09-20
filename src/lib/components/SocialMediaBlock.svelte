3aa7a39d41e838ed972448f9186eba167fc4cf86
Saif <ninja_saif@hotmail.com>
2025-09-20 03:16:27 +0200

    feat: add SocialMediaBlock with icons #7

---
+++ src/lib/components/SocialMediaBlock.svelte
@@ -0,0 +1,65 @@
+<script>
+  import EmailIcon from "$lib/assets/icons/EmailIcon.svelte";
+  import EmailHoverIcon from "$lib/assets/icons/EmailHoverIcon.svelte";
+  import DiscordIcon from "$lib/assets/icons/DiscordIcon.svelte";
+  import DiscordHoverIcon from "$lib/assets/icons/DiscordHoverIcon.svelte";
+  import TwitterIcon from "$lib/assets/icons/TwitterIcon.svelte";
+  import TwitterHoverIcon from "$lib/assets/icons/TwitterHoverIcon.svelte";
+  // import GithubIcon from "$lib/assets/icons/GithubIcon.svelte";
+  // import GithubHoverIcon from "$lib/assets/icons/GithubHoverIcon.svelte";
+</script>
+
+<section class="email-container">
+  <ul class="icon-list">
+    <li class="icon-wrapper">
+      <EmailIcon className="default-icon" />
+      <EmailHoverIcon className="hover-icon" />
+    </li>
+    <li class="icon-wrapper">
+      <DiscordIcon className="default-icon" />
+      <DiscordHoverIcon className="hover-icon" />
+    </li>
+    <li class="icon-wrapper">
+      <TwitterIcon className="default-icon" />
+      <TwitterHoverIcon className="hover-icon" />
+    </li>
+    <!-- <li class="icon-wrapper">
+      <GithubIcon className="default-icon" />
+      <GithubHoverIcon className="hover-icon" />
+    </li> -->
+  </ul>
+</section>
+
+<style>
+  .icon-list {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+  }
+
+  .icon-wrapper {
+    position: relative;
+    display: inline-block;
+    width: 50px;
+    height: 50px;
+    cursor: pointer;
+  }
+
+  .icon-wrapper :global(.default-icon),
+  .icon-wrapper :global(.hover-icon) {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    transition: opacity 0.2s ease;
+  }
+
+  .icon-wrapper :global(.hover-icon) {
+    opacity: 0;
+  }
+
+  .icon-wrapper:hover :global(.hover-icon) {
+    opacity: 1;
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,5 @@
-<h1>Your tribe for life profile card</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+  import SocialMediaBlock from "$lib/components/SocialMediaBlock.svelte";
+</script>
+
+<SocialMediaBlock/>
