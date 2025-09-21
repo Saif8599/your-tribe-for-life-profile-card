a2590f4b7c517e7d2489d4b1e089869215533673
Saif <ninja_saif@hotmail.com>
2025-09-21 20:54:21 +0200

    feat: add carousel with buttons and markers #7

--- src/lib/components/ModalBlock.svelte
+++ src/lib/components/ModalBlock.svelte
@@ -3,12 +3,42 @@
 </script>
 
 <dialog id="profile-card" class="profile-card" popover>
-  dit is een dialog popover
+  <div class="carousel carousel--scroll-buttons carousel--scroll-markers">
+    <li class="carousel-slide">
+      <!-- Fallback -->
+      <img
+        src="src\lib\assets\images\Character-1.png"
+        alt="Character-2"
+        loading="lazy"
+        class="character-1"
+      />
+    </li>
+    <li class="carousel-slide">
+      <!-- Fallback -->
+      <img
+        src="src\lib\assets\images\Character-2.png"
+        alt="Character-2"
+        loading="lazy"
+        class="character-2"
+      />
+    </li>
+    <li class="carousel-slide">
+      <!-- Fallback -->
+      <img
+        src="src\lib\assets\images\Character-3.png"
+        alt="Character-3"
+        loading="lazy"
+        class="character-3"
+      />
+    </li>
+  </div>
+
   <!-- <SocialMediaBlock /> -->
 </dialog>
 <button class="dialog-button" popovertarget="profile-card">Click</button>
 
 <style>
+  /* Profile card */
   .profile-card {
     position: fixed;
     margin: auto;
@@ -23,6 +53,74 @@
     display: none;
   }
 
+  /* Carousel */
+
+  .carousel {
+    display: flex;
+    width: 100%;
+    overflow-x: auto;
+    overscroll-behavior-x: contain;
+    scroll-snap-type: x mandatory;
+    scroll-behavior: smooth;
+    anchor-name: --carousel;
+    text-align: center;
+    scroll-marker-group: after;
+  }
+  .carousel-slide {
+    flex: 0 0 100%;
+    scroll-snap-align: center;
+  }
+
+  .carousel--scroll-buttons {
+    &::scroll-button(*) {
+      position: fixed;
+      position-anchor: --carousel;
+      cursor: pointer;
+      border: none;
+      background-color: transparent;
+    }
+
+    &::scroll-button(right) {
+      position-area: inline-end center;
+      content: url(../assets/icons/ArrowRight.svg);
+      translate: -100%;
+    }
+    &::scroll-button(right):disabled {
+      opacity: 0;
+    }
+
+    &::scroll-button(left) {
+      position-area: inline-start center;
+      content: url(../assets/icons/ArrowLeft.svg);
+      translate: 100%;
+    }
+    &::scroll-button(left):disabled {
+      opacity: 0;
+    }
+  }
+
+  .carousel::scroll-marker-group {
+    display: flex;
+    justify-content: center;
+    gap: 0.5em;
+  }
+
+  .carousel-slide::scroll-marker {
+    content: "";
+    height: 10px;
+    width: 10px;
+    border-radius: 50%;
+    border: 1px solid var(--text-color);
+  }
+
+  .carousel-slide::scroll-marker:target-current {
+    background-color: var(--text-color);
+  }
+
+  ::-webkit-scrollbar {
+    width: 0;
+  }
+
   .dialog-button {
     position: fixed;
     margin: auto;
@@ -35,6 +133,8 @@
     cursor: pointer;
     font-size: var(--font-size-mobile-l);
   }
+
+  /* Media query */
 
   @media (min-width: 765px) {
     .profile-card {
